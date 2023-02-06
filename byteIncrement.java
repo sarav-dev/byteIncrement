f2043a9bbcf8cc0751ca7e42ba95c908eda32e38
unknown <webdsnr_sarav@hotmail.com>
2023-02-07 00:01:50 +0530

    first commit

---
+++ byteIncrement.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class byteIncrement {
+    public static void main(String args[])
+    {   
+         byte by = 127; 
+         by++;
+         System.out.println(by);
+    }
+}
